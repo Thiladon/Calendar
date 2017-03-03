c6efcce0f208d904fe568ec286928baae90390b8
Marchand Alexis <21608957@n304l-128013.etu.info.unicaen.fr>
2017-03-03 12:21:23 +0100

    Ajout du calendrier

---
+++ CalendrierJS/script.js
@@ -0,0 +1,16 @@
+(function(){
+	var Calc = function() {}
+	
+	Calc.prototype.initialize = function() {
+		today = new Date();
+		this.month = (today.getMonth() + 1) - 2 < 1 ? 10 + (today.getMonth + 1) : (today.getMonth() + 1) - 2;
+		this.century = (Math.floor((today.getFullYear()/10)%10) + (today.getFullYear()%10));
+		this.day = today.getDate();
+		
+		this.run();
+	}
+	
+	Calc.prototype.run = function() {
+		
+	}
+}
